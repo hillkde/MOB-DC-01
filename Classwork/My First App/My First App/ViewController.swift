23079994f5a147e63a373bbcc07747b9557b534e
Kathryn Hill <hillkde@gmail.com>
2014-12-17 21:13:55 -0500

    Bulldog App

---
+++ Classwork/My First App/My First App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  My First App
+//
+//  Created by Kathryn Hill on 12/17/14.
+//  Copyright (c) 2014 Kathryn Hill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
