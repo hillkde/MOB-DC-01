ae01d01f2e45b54f09f085e986fdeb4262ba418f
Kathryn Hill <hillkde@gmail.com>
2015-02-14 13:59:21 -0500

    Commit 14 FEB 2015

--- Homework/Hangman KDH/Hangman KDH/SecondViewController.swift
+++ Homework/Hangman KDH/Hangman KDH/SecondViewController.swift
@@ -9,37 +9,38 @@
 import UIKit
 
 protocol Hangman {
+//    how I get the Letter and Wrong guesses to add to different arrays in VC?
     
-    func currentWord(letter: String) {
-        self.correctGuesses.append(letter)
-        return self.correctGuesses.count
+    func addLetterToArray(letter: String) {
+    self.correctGuesses.append(letter)
+    return self.correctGuesses.count
+    }
+    
+    func wrongGuessesToArray(letter: String) {
+    self.wrongGuesses.append(letter)
+    return self.wrongGuesses.count
     }
 }
 
 class SecondViewController: UIViewController {
     
+    @IBOutlet weak var guessTextBox: UITextField!
     
+    var delegate: Hangman?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-
+    }
+    
+    @IBAction func guessAndGoBack(sender: UIButton) {
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- Homework/Hangman KDH/Hangman KDH/ViewController.swift
+++ Homework/Hangman KDH/Hangman KDH/ViewController.swift
@@ -12,8 +12,24 @@ import UIKit
 
 class ViewController: UIViewController, Hangman {
     
+    @IBOutlet weak var correctLabel: UILabel!
+    
+    @IBOutlet weak var correctText: UITextField!
+    
+    @IBOutlet weak var wrongGuessLabel: UILabel!
+
+    @IBOutlet weak var remainingText: UITextField!
+    
+    
     var correctGuesses = ["s", "w", "i", "f", "t"]
     var incompleteWord = ""
+    var wrongGuesses = ""
+    var myWord = ""
+    var wrongGuesses.count < 7
+    
+    if myWord.lowerCaseString.rangeOfString("s", "w", "i", "f", "t")! = nil {
+    return ("Swift")
+    }
     
     for letter in myWord {
     if contains(correctGuesses, String(letter).lowerCaseString){
@@ -24,12 +40,8 @@ class ViewController: UIViewController, Hangman {
     incompleteWord += "_"
     }
     
-    }
+    println("Swift")
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        var greyBox = UIView(frame: CGRect(x: 20, y: 20, width: 20
     }
     
     @IBAction func playGameButton(sender: AnyObject) {
@@ -40,19 +52,82 @@ class ViewController: UIViewController, Hangman {
         self.presentViewController(guessVC, animated: true, completion: nil)
     }
     
-    func currentWord(letter: String) {
-        self.correctGuesses.append(letter)
-        return self.correctGuesses.count
+        func addLetterToArray(letter: String) {
+            self.correctGuesses.append(letter)
+            return self.correctGuesses.count
+        
+        func wrongGuessesToArray(letter: String) {
+            self.wrongGuesses.append(letter)
+            return self.wrongGuesses.count
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var firstBox = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
+        firstBox.backgroundColor = UIColor.grayColor()
+        self.view.addSubview(firstBox)
+        
+        var secondBox = UIView(frame: CGRect(x: 25, y: 0, width: 20, height: 20))
+        secondBox.backgroundColor = UIColor.grayColor()
+        self.view.addSubview(secondBox)
+        
+        var thirdBox = UIView(frame: CGRect(x: 50, y: 0, width: 20, height: 20))
+        thirdBox.backgroundColor = UIColor.grayColor()
+        self.view.addSubview(thirdBox)
+        
+        var fourthBox = UIView(frame: CGRect(x: 75, y: 0, width: 20, height: 20))
+        fourthBox.backgroundColor = UIColor.grayColor()
+        self.view.addSubview(fourthBox)
+        
+        var fifthBox = UIView(frame: CGRect(x: 100, y: 0, width: 20, height: 20))
+        fifthBox.backgroundColor = UIColor.grayColor()
+        self.view.addSubview(fifthBox)
+        
+        var sixthBox = UIView(frame: CGRect(x: 125, y: 0, width: 20, height: 20))
+        sixthBox.backgroundColor = UIColor.grayColor()
+        self.view.addSubview(sixthBox)
+        
+        var seventhBox = UIView(frame: CGRect(x: 150, y: 0, width: 20, height: 20))
+        seventhBox.backgroundColor = UIColor.grayColor()
+        self.view.addSubview(seventhBox)
+        
+        if wrongGuesses.count == 1 {
+            self.firstBox.backgroundColor = UIColor.redColor()
+        }
+        
+        if wrongGuesses.count == 2 {
+            self.secondBox.backgroundColor = UIColor.redColor()
+        }
+        
+        if wrongGuesses.count == 3 {
+            self.thirdBox.backgroundColor = UIColor.redColor()
+        }
+        
+        if wrongGuesses.count == 4 {
+            self.fourthBox.backgroundColor = UIColor.redColor()
+        }
+        
+        if wrongGuesses.count == 5 {
+            self.fifthBox.backgroundColor = UIColor.redColor()
+        }
+        
+        if wrongGuesses.count == 6 {
+            self.fifthBox.backgroundColor = UIColor.redColor()
+        }
+        
+        if wrongGuesses.count == 7 {
+            self.fifthBox.backgroundColor = UIColor.redColor()
+            println("Game Over")
+        }
+
+        
     }
     
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-       
-        
-        
     }
-
 
 }
 
